e1d72df00c12b36fb676cf94d5eb7ca469e8d5d2
Raphael Bernhard <beraphae@gmail.com>
2020-02-18 22:49:15 +0100

    html_macro: Write down some ideas

--- html_macro/src/lib.rs
+++ html_macro/src/lib.rs
@@ -1,3 +1,40 @@
+///
+/// This crates complements greenhorn by adding a html!{} macro
+/// to simplify creating view functions:
+///
+/// ```
+/// struct Main {
+/// }
+///
+/// enum Msg {
+///     MouseDown(DomEvent),
+/// }
+///
+/// impl Main {
+///     fn nested_view_function(&self) -> Node<Msg> {
+///         html!{ <p>Example paragraph</p> }
+///     }
+/// }
+///
+///
+/// impl Render for Main {
+///     type Message = Msg;
+///
+///    fn render(&self) -> Node<Self::Message> {
+///         let mouse_move_js_handler = "function(evt) { console.log(evt); }";
+///         html!{
+///             <div .class_name #my_id other_attribute="attribute_value" @mousemove=mouse_move_js_handler @mousedown={Msg::MouseDown}>
+///                 Some text <{mount(self.component, Msg::ComponentMsg)} /> and more text... and {"some rust code"}
+///                 {self.nested_view_function()}
+///             </div>
+///         }
+///     }
+/// }
+///
+/// ```
+///
+
+
 mod tree;
 
 extern crate proc_macro;
--- html_macro/src/tree.rs
+++ html_macro/src/tree.rs
@@ -2,6 +2,12 @@ use syn::parse::{Parse, ParseStream};
 use syn::parse::Result as SynResult;
 use quote::ToTokens;
 use proc_macro2::TokenStream;
+use syn::buffer::Cursor;
+
+
+trait Match {
+    fn matches(cursor: Cursor) -> bool;
+}
 
 pub struct Tree {
 
