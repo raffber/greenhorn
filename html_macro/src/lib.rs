dd0d78a16f76a8203aadf6f6ee5d30cc6f1a9250
Raphael Bernhard <beraphae@gmail.com>
2020-02-23 13:25:56 +0100

    Add proc_macro_error to html_macro crate

--- html_macro/src/lib.rs
+++ html_macro/src/lib.rs
@@ -43,10 +43,12 @@ use proc_macro::TokenStream;
 use proc_macro_hack::proc_macro_hack;
 use syn::parse_macro_input;
 use quote::quote;
-use tree::Tree;
+use tree::Element;
+use proc_macro_error::proc_macro_error;
 
+#[proc_macro_error]
 #[proc_macro_hack]
 pub fn html(input: TokenStream) -> TokenStream {
-    let root = parse_macro_input!(input as Tree);
+    let root = parse_macro_input!(input as Element);
     TokenStream::from(quote! {#root})
 }
--- html_macro/src/tree.rs
+++ html_macro/src/tree.rs
@@ -3,23 +3,30 @@ use syn::parse::Result as SynResult;
 use quote::ToTokens;
 use proc_macro2::TokenStream;
 use syn::buffer::Cursor;
+use proc_macro_error::*;
 
 
 trait Match {
     fn matches(cursor: Cursor) -> bool;
 }
 
-pub struct Tree {
+pub struct Element {
 
 }
 
-impl Parse for Tree {
+impl Match for Element {
+    fn matches(cursor: Cursor) -> bool {
+        unimplemented!()
+    }
+}
+
+impl Parse for Element {
     fn parse(input: ParseStream) -> SynResult<Self> {
         unimplemented!()
     }
 }
 
-impl ToTokens for Tree {
+impl ToTokens for Element {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         unimplemented!()
     }
