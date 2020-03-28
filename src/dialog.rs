cde350c14774b8b7da59b7e5be689f03067a4f04
Raphael Bernhard <beraphae@gmail.com>
2020-03-28 22:09:18 +0100

    dialog: more work

--- src/dialog.rs
+++ src/dialog.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use std::marker::PhantomData;
 
 mod private {
     use super::*;
@@ -8,11 +9,15 @@ mod private {
     impl Sealed for FileOpenDialog {}
     impl Sealed for MultipleFileOpenDialog {}
     impl Sealed for MessageBox {}
+    impl Sealed for FileSaveDialog {}
 }
 
 pub trait Dialog<'de>: private::Sealed + Serialize + Deserialize<'de> {
     type Msg: Serialize + Deserialize<'de>;
 
+    /// Called by the runtime to produce a result based on the received
+    /// data from the dialog after the user has closed it.
+    fn resolve(self, data: &str) -> Self::Msg;
 }
 
 
@@ -34,6 +39,10 @@ impl FileOpenDialog {
 
 impl Dialog<'_> for FileOpenDialog {
     type Msg = FileOpenMsg;
+
+    fn resolve(self, data: &str) -> Self::Msg {
+        unimplemented!()
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -49,6 +58,9 @@ pub struct MultipleFileOpenDialog {}
 impl Dialog<'_> for MultipleFileOpenDialog {
     type Msg = MultipleFileOpenMsg;
 
+    fn resolve(self, data: &str) -> Self::Msg {
+        unimplemented!()
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -66,7 +78,42 @@ pub struct MessageBox {
 
 impl Dialog<'_> for MessageBox {
     type Msg = MessageBoxMsg;
+
+    fn resolve(self, data: &str) -> Self::Msg {
+        unimplemented!()
+    }
 }
 
+
+
+#[derive(Serialize, Deserialize)]
+pub enum FileSaveMsg {
+    SaveTo(String),
+    Cancel()
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FileSaveDialog {
+
+}
+
+impl Dialog<'_> for FileSaveDialog {
+    type Msg = FileSaveMsg;
+
+    fn resolve(self, data: &str) -> Self::Msg {
+        unimplemented!()
+    }
+}
+
+
+pub(crate) struct DialogBinding<T: Send + 'static> {
+    phantom: PhantomData<T>,
+}
+
+impl<T: Send + 'static> DialogBinding<T> {
+    pub(crate) fn new<'de, D: Dialog<'de>, F: Fn(D::Msg) -> T>(&self, _dialog: D, _fun: F) -> Self {
+        todo!()
+    }
+}
 
 
