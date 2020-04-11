81caf383e8bfb8ede74f931d7548b335006f6cb8
Raphael Bernhard <beraphae@gmail.com>
2020-04-11 22:04:38 +0200

    Document blob

--- src/blob.rs
+++ src/blob.rs
@@ -33,7 +33,40 @@ pub(crate) struct BlobData {
 /// ## Example
 ///
 /// ```
-/// // TODO ...
+/// # use greenhorn::blob::Blob;
+/// # use greenhorn::Render;
+/// # use greenhorn::node::Node;
+///
+/// struct MyImage {
+///     blob: Blob,
+/// }
+///
+/// impl MyImage {
+///     fn new() -> Self {
+///         // hash can be constant, if we don't plan to change the content
+///         let blob = Blob::build(0)
+///             .mime_type("image/png")
+///             .data(vec![1,2,3])      // add image data here!
+///             .build();
+///         MyImage { blob }
+///     }
+/// }
+///
+/// impl Render for MyImage {
+///     type Message = ();
+///
+///     fn render(&self) -> Node<Self::Message> {
+///         let js = format!("{{
+///             var blob = app.getBlob({});
+///             var img_url = URL.createObjectURL(blob.blob);
+///             event.target.src = img_url;
+///         }}", self.blob.id().data());
+///         Node::html().elem("img")
+///             .js_event("render", js)
+///             .add(&self.blob)
+///             .build()
+///     }
+/// }
 /// ```
 ///
 #[derive(Clone)]
@@ -53,26 +86,68 @@ impl Blob {
         }
     }
 
+    /// Returns the id associated with this blob.
+    ///
+    /// Note that the id and the hash define a unique key to the `Blob`.
+    /// The `id` should define the blob within the application data-flow and is usually
+    /// auto-generated.
     pub fn id(&self) -> Id {
         self.inner.id
     }
 
+    /// Returns the hash of the data associated with this blob.
+    ///
+    /// Note that the id and the hash define a unique key to the `Blob`.
+    /// The `hash` should represent the version of the data underlying the `Blob`.
+    /// Thus, if the `hash` changes, the `Blob` is considered changed and re-transmitted to
+    /// the frontend.
     pub fn hash(&self) -> u64 {
         self.inner.hash
     }
 
+    /// Returns the data underlying the Blob
     pub fn data(&self) -> &Vec<u8> {
         &self.inner.data
     }
 
+    /// Returns the mime-type of the data
     pub fn mime_type(&self) -> &str {
         &self.inner.mime_type
     }
 
+    /// Returns the registered javascript function which is called if the content of
+    /// the `Blob` changes, i.e. if the hash of the `Blob` changes but the `Id` remains the same.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use greenhorn::node::Node;
+    /// # use greenhorn::Id;
+    /// # use greenhorn::blob::Blob;
+    ///
+    /// fn render_blob() -> Blob {
+    ///     let blob_id = Id::new();
+    ///     let js = format!("{{
+    ///         var elem = document.getElementById('my-html-id');
+    ///         var blob = app.getBlob({});
+    ///         var img_url = URL.createObjectURL(blob.blob);
+    ///         elem.src = img_url;
+    ///     }}", blob_id.data());
+    ///
+    ///     Blob::build(0)
+    ///         .data(vec![1,2,3])
+    ///         .mime_type("image/png")
+    ///         .id(blob_id)
+    ///         .on_change(js)
+    ///         .into()
+    /// }
+    /// ```
     pub fn on_change(&self) -> Option<&str> {
         self.inner.on_change.as_deref()
     }
 
+    /// Returns the registered javascript function which is called when the `Blob` is
+    /// added to the frontend.
     pub fn on_add(&self) -> Option<&str> {
         self.inner.on_add.as_deref()
     }
