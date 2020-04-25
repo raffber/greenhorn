f5ca3813875c0dab6a26a61031e4b6bb98353f71
Raphael Bernhard <beraphae@gmail.com>
2020-04-25 14:08:33 +0200

    wasm: more setup

--- examples/wasm/js/index.js
+++ examples/wasm/js/index.js
@@ -1,2 +1,17 @@
 
-import('../pkg')
+import('../pkg').then(module => {
+    window.push_string = (arg) => {
+        console.log("push_string")
+        // module.js_to_wasm('{ "Dialog": [] }');
+        console.log(arg)
+    };
+
+
+    window.push_binary = (arg) => {
+        console.log("push_binary")
+        console.log(arg)
+    };
+
+
+    module.start();
+}).catch(console.error)
--- examples/wasm/src/lib.rs
+++ examples/wasm/src/lib.rs
@@ -1,4 +1,10 @@
 use wasm_bindgen::prelude::*;
+use futures::StreamExt;
+use greenhorn::wasm_pipe::WasmPipe;
+use greenhorn::prelude::*;
+use greenhorn::html;
+use greenhorn::platform::spawn;
+use greenhorn::pipe::{TxMsg, Pipe};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -17,6 +23,45 @@ pub fn set_panic_hook() {
         console_error_panic_hook::set_once();
 }
 
+
+struct MyApp {
+    state: i32,
+}
+
+impl Render for MyApp {
+    type Message = ();
+
+    fn render(&self) -> Node<Self::Message> {
+        html!(
+            <div>{"Hello, World!!"}</>
+        ).into()
+    }
+}
+
+impl App for MyApp {
+    fn update(&mut self, _msg: Self::Message, _ctx: Context<Self::Message>) -> Updated {
+        Updated::yes()
+    }
+}
+
+#[wasm_bindgen]
+pub fn start() {
+    set_panic_hook();
+    println!("test");
+    let pipe = WasmPipe::new();
+    let app = MyApp { state: 123 };
+    let (rt, _control) = Runtime::new(app, pipe);
+    spawn(rt.run());
+    // let (tx, rx) = pipe.split();
+    // tx.0.unbounded_send(TxMsg::Ping());
+    // spawn(async move {
+    //     let mut rx = rx;
+    //     while let Some(x) = rx.next().await {
+    //         alert("hia!");
+    //     }
+    // });
+}
+
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
@@ -24,5 +69,5 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, {{project-name}}!");
+    alert("foo!!!");
 }
