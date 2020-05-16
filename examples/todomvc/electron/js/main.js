128d4cca7f36499923e1d55693b9704d708fb099
Raphael Bernhard <beraphae@gmail.com>
2020-05-16 16:08:32 +0200

    Add electron deploy example

---
+++ examples/todomvc/electron/js/main.js
@@ -0,0 +1,38 @@
+import addon from 'node-loader!../native/index.node';
+
+const {app, BrowserWindow, Menu} = require('electron');
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
+
+function createWindow () {
+  const port = addon.run();
+  console.log('PORT' + port);
+  global.port = port;
+
+  Menu.setApplicationMenu(null);
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+      app.quit();
+  }
+})
