a8eae39dea0820247d55aac28493fa385a607f91
Raphael Bernhard <beraphae@gmail.com>
2020-05-23 17:53:01 +0200

    API documentation

--- src/runtime/metrics.rs
+++ src/runtime/metrics.rs
@@ -1,3 +1,13 @@
+//! This modules allows collecting runtime information about a greenhorn application
+//!
+//! The [Metrics](struct.Metrics.html) object collects performance information during runtime.
+//! The [Runtime](../struct.Runtime.html) objects maintains such a `Metrics` objects and returns
+//! it after running the application to completion.
+//!
+//! The performance data is collected in a histogram. Refer to the [Histogram](struct.Histogram.html)
+//! object for details.
+//!
+
 use crate::Id;
 use hdrhistogram::Histogram as HdrHistogram;
 use hdrhistogram::{CreationError, RecordError};
@@ -10,18 +20,25 @@ use std::io;
 use std::result::Result as StdResult;
 use std::time::Duration;
 
-// newtype for histogram to impl Serialize
+/// Histogram type to collect performance information as u64.
+///
+/// This is just a new-type for `HdrHistogram` to implement a custom
+/// `serde::Serialize`.
+/// Refer to [HdrHistogram](https://docs.rs/hdrhistogram/7.1.0/hdrhistogram/struct.Histogram.html).
 struct Histogram(HdrHistogram<u64>);
 
 impl Histogram {
+    /// Refer to [HdrHistogram::new_with_bounds](https://docs.rs/hdrhistogram/7.1.0/hdrhistogram/struct.Histogram.html#method.new_with_bounds).
     pub fn new_with_bounds(low: u64, high: u64, sigfig: u8) -> Result<Self, CreationError> {
         HdrHistogram::new_with_bounds(low, high, sigfig).map(Histogram)
     }
 
+    /// Refer to [HdrHistogram::record_n](https://docs.rs/hdrhistogram/7.1.0/hdrhistogram/struct.Histogram.html#method.record_n).
     pub fn record_n(&mut self, value: u64, count: u64) -> Result<(), RecordError> {
         self.0.record_n(value, count)
     }
 
+    /// Refer to [HdrHistogram::record](https://docs.rs/hdrhistogram/7.1.0/hdrhistogram/struct.Histogram.html#method.record).
     pub fn record(&mut self, value: u64) -> Result<(), RecordError> {
         self.0.record(value)
     }
@@ -59,15 +76,15 @@ impl Serialize for Histogram {
     }
 }
 
-trait Metric<'de>: Serialize {
+/// Marks a serializeable type which supplies a [Histogram](struct.Histogram.html)
+trait Metric: Serialize {
     fn histogram(&self) -> &Histogram;
 }
 
-#[derive(Serialize)]
-pub struct ResponseTime {
-    hist: Histogram,
-}
-
+/// Allows measuring hit count per second of a function.
+///
+/// Data is collected in hits per second. The measurement
+/// range is from 1 to 1000 hits per second.
 #[derive(Serialize)]
 pub struct Throughput {
     hist: Histogram,
@@ -80,6 +97,7 @@ pub struct Throughput {
 }
 
 impl Throughput {
+    /// Create a new `Throughput` object.
     pub fn new() -> Self {
         Self {
             hist: Histogram::new_with_bounds(1, 1000, 3).unwrap(),
@@ -88,12 +106,14 @@ impl Throughput {
         }
     }
 
+    /// Record a hit
     pub fn hit(&mut self) {
         self.update();
         self.last_count += 1;
     }
 
-    pub fn update(&mut self) {
+    /// Update the histogram
+    fn update(&mut self) {
         let now = Instant::now();
         if let Some(last_update) = self.last_update {
             let delta = now.duration_since(last_update).as_secs_f64();
@@ -118,19 +138,31 @@ impl Default for Throughput {
     }
 }
 
-impl Metric<'_> for Throughput {
+impl Metric for Throughput {
     fn histogram(&self) -> &Histogram {
         &self.hist
     }
 }
 
+
+/// Allows measures execution time of a function.
+///
+/// Data is collected in us. The supported execution time range is from
+/// 1us to 1s.
+#[derive(Serialize)]
+pub struct ResponseTime {
+    hist: Histogram,
+}
+
 impl ResponseTime {
+    /// Create a new `ResponseTime` object.
     pub fn new() -> Self {
         Self {
             hist: Histogram::new_with_bounds(1, 1e6 as u64, 3).unwrap(),
         }
     }
 
+    /// Run a closure and measures it's execution time
     pub fn run<T, F: FnOnce() -> T>(&mut self, fun: F) -> T {
         let before = Instant::now();
         let ret = fun();
@@ -141,6 +173,7 @@ impl ResponseTime {
         ret
     }
 
+    /// Record an execution time in the underlying histogram
     pub fn record(&mut self, delta: Duration) {
         self.hist.record(delta.as_micros() as u64).unwrap();
     }
@@ -152,6 +185,10 @@ impl Default for ResponseTime {
     }
 }
 
+/// Collects `render()` performance information about a [Component](../struct.Component.html).
+///
+/// Records how often the a function of a `Component` is called and records the time
+/// each call requires to complete.
 #[derive(Serialize, Default)]
 pub struct ComponentMetric {
     time: ResponseTime,
@@ -163,6 +200,7 @@ impl ComponentMetric {
         Default::default()
     }
 
+    /// Measure and execute a function and collect throughput and response time information.
     pub fn run<T, F: FnOnce() -> T>(&mut self, fun: F) -> T {
         let ret = self.time.run(fun);
         self.throughput.hit();
@@ -170,20 +208,32 @@ impl ComponentMetric {
     }
 }
 
+/// Aggregation of metrics collected during execution of a [Runtime](struct.Runtime.html) object.
 #[derive(Serialize, Default)]
 pub struct Metrics {
+    /// `render()` performance and hit count for each component
     pub components: HashMap<Id, ComponentMetric>,
+
+    /// `render()` performance and hit count for the root component
     pub root: ComponentMetric,
+
+    /// Collects the response time of creating a patch after rendering a VDom
     pub diff: ResponseTime,
+
+    /// Collects the time required to diff the VDom which resulted in an empty patch
+    ///
+    /// This condition might be avoided by correctly reporting whether a component should
+    /// re-render using an [Updated](struct.Updated.html) object.
     pub empty_patch: ResponseTime,
 }
 
 impl Metrics {
-    pub fn new() -> Self {
+    pub(crate) fn new() -> Self {
         Default::default()
     }
 
-    pub fn run_comp<T, F>(&mut self, id: Id, fun: F) -> T
+    /// Run a function and record its execution time in the component with the associated `id`.
+    pub(crate) fn run_comp<T, F>(&mut self, id: Id, fun: F) -> T
     where
         F: FnOnce() -> T,
     {
@@ -196,6 +246,7 @@ impl Metrics {
         metric.run(fun)
     }
 
+    /// JSON serialize this object.
     pub fn write(&self, out: impl io::Write) -> StdResult<(), String> {
         serde_json::to_writer(out, self).map_err(|x| format!("{}", x))
     }
