daf7b6822831d8d606b5cbab16526938c32b0219
raffber <beraphae@gmail.com>
2021-05-22 16:42:30 +0000

    Deploying to gh-pages from  @ bae3259ecfaf4d7e576db3038d7d23c35675c3da 🚀

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["greenhorn"] = [{"text":"impl Eq for Id","synthetic":false,"types":[]}];
+implementors["greenhorn"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"greenhorn/struct.Id.html\" title=\"struct greenhorn::Id\">Id</a>","synthetic":false,"types":["greenhorn::Id"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["greenhorn"] = [{"text":"impl Hash for Id","synthetic":false,"types":[]}];
+implementors["greenhorn"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"greenhorn/struct.Id.html\" title=\"struct greenhorn::Id\">Id</a>","synthetic":false,"types":["greenhorn::Id"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
